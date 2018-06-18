b1fadceaeb97a8720e109c0e75a4b4b695aba761
Zhiqiang Zhou <zz4716@matrix13.doc.ic.ac.uk>
2018-06-18 13:17:18 +0100

    implement method to extract feature map from h5 file

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import time
-import os
 import csv
 from scipy import misc
 import numpy as np
 import h5py
 from god_config import *
+import tensorflow as tf
+from vgg19_model import VGG19
+import copy
 
 def read_images(print_not_found=False):
     # Read images ids from csv file
@@ -82,6 +84,98 @@ def save_dnn_feature_map(image_ids, features, all_layers=True):
             for j in range(len(LAYER_TO_BE_SAVED)):
                 grp.create_dataset(LAYER_TO_BE_SAVED[j], data=features[j][i])
     print ("Data saved. takes %.2fs" %(time.time() - start_time))
+
+def extract_dnn_features():
+    data = read_images()
+    data_shape = data["rescaled_images"].shape
+    batch_size = 15
+    num_of_batch = int(data_shape[0]/batch_size)+1
+
+    save_every = 4
+
+    inputs = tf.placeholder("float", (None, 224, 224, 3))
+
+    vgg19 = VGG19()
+
+    with tf.name_scope("vgg_content"):
+        vgg19.build(inputs)
+    layers_so_far = []
+    images_id_so_far = []
+    for i in range(num_of_batch):
+        if i == num_of_batch - 1:
+            rgb = (data["rescaled_images"][i*batch_size:, :, :, :]).astype("float32")
+            images_id_so_far += data["image_ids"][0][i*batch_size:]
+        else:
+            rgb = (data["rescaled_images"][i*batch_size:(i+1)*batch_size,:, :, :]).astype("float32")
+            images_id_so_far += data["image_ids"][0][i*batch_size:(i+1)*batch_size]
+
+        feed_dict = {inputs: rgb}
+        start_time = time.time()
+
+        with tf.Session() as sess:
+            print ("Start to evaluate batch {}/{}".format(i+1, num_of_batch))
+            layers = sess.run([vgg19.conv1_2, vgg19.conv2_2,
+                               vgg19.conv3_4,
+                               vgg19.conv4_4,
+                               vgg19.conv5_4,
+                               vgg19.fc6, vgg19.fc7, vgg19.fc8],
+                              feed_dict=feed_dict)
+
+            print ("Time spent : %.5ss" %(time.time()-start_time))
+
+            if len(layers_so_far) == 0:
+                layers_so_far = copy.deepcopy(layers)
+            else:
+                if len(layers_so_far) != len(layers):
+                    raise Exception('Configurations doesn\'t match previous')
+                else:
+                    for j in range(len(layers_so_far)):
+                        layers_so_far[j] = np.concatenate((layers_so_far[j], layers[j]) , axis=0)
+        if (i+1) % save_every == 0 or i == num_of_batch-1:
+            save_dnn_feature_map(features=layers_so_far, image_ids=images_id_so_far, all_layers=False)
+            layers_so_far = []
+            images_id_so_far = []
+
+def get_dnn_features_by_imageid_and_layer(
+        imageids, layers=None, all_layers=False):
+    if all_layers:
+        if not os.path.isfile(os.path.join( IMAGE_FEATURES_FILE_NAME_FULL)):
+            raise Exception("Image feature file doesnt exist!")
+        else:
+            file_path = os.path.join(RESULT_DIR, IMAGE_FEATURES_FILE_NAME_FULL)
+    else:
+        if not os.path.isfile(os.path.join(RESULT_DIR, IMAGE_FEATURES_FILE_NAME_LESS)):
+            raise Exception("Image feature file doesnt exist!")
+        else:
+            file_path = os.path.join(RESULT_DIR, IMAGE_FEATURES_FILE_NAME_LESS)
+
+    if len(imageids) == 0:
+        raise Exception("Image ids to be retrieved is none.")
+
+    ret = {}
+    with h5py.File(file_path, 'r') as f:
+        for imageid in imageids:
+            if imageid not in f:
+                print ("{} is not in dataset.".format(imageid))
+                continue
+            else:
+                if layers == None:
+                    tmp_dict = {}
+                    for key in list(f[imageid]):
+                        tmp_dict[key] = np.array(f[imageid][key])
+                    ret[imageid] = tmp_dict
+                else:
+                    tmp_dict = {}
+                    for layer in layers:
+                        if layer not in f[imageid]:
+                            print ("Layer {} cannot be found in image {}".format(layer, imageid))
+                        else:
+                            tmp_dict[layer] = np.array(f[imageid][layer])
+                    ret[imageid] = tmp_dict
+
+    return ret
+
+
 
 
 
