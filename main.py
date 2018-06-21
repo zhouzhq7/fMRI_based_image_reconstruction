2aafd5369b6bf578b35bf8ca1dafd7cff85b909b
zhouzhq7 <zhouzhq7@gmail.com>
2018-06-21 14:25:55 +0100

    choose another image to reconstruct

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def main():
     with open('tmp.pkl', 'rb') as f:
         tmp = pickle.load(f)
 
-    #for key in tmp.keys():
-    key = 'conv2_2'
-    reshaped_target = tmp[key].reshape([1]+list(tmp[key].shape))
-    with tf.Graph().as_default():
-        recon_image_by_given_layer(reshaped_target, key)
+    for key in tmp.keys():
+        if key == 'conv1_1' or key == 'conv2_2':
+            reshaped_target = tmp[key].reshape([1]+list(tmp[key].shape))
+            with tf.Graph().as_default():
+                recon_image_by_given_layer(reshaped_target, key)
 
 if __name__=="__main__":
     main()
